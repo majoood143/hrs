a2ab633f5c34133a66d68f4cf235717056690d2e
majid al hajri <m.m.h.89@hotmail.com>
2025-03-20 23:35:38 +0400

    trying to add it again

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,6 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     function index() : View {
-        return view('admin.layouts.master');
+        return view('admin.dashboard');
     }
 }
